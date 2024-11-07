7bfad5277d274e14815013d26e03c673f5d0d3ca
AliceLiu2024 <ucfnicg@ucl.ac.uk>
2024-11-07 14:10:14 +0000

    update homework4

--- wk4/wk4_homework.R
+++ wk4/wk4_homework.R
@@ -1,49 +1,47 @@
-##load library
+## load packages
 library(tmap)
 library(sf)
 library(dplyr)
 library(janitor)
+library(countrycode)
 
-##load data
-countries <- st_read("wk4/World_Countries_Generalized_Shapefile/World_Countries_Generalized.shp")
-gii <-  read.csv("wk4/gii_2010-2019.csv")
 
-##edit data
-gii <- gii %>%
-  clean_names()
-countries <- countries %>%
-  clean_names()
-gii2 <- gii %>%
-  mutate(difference = gii_2019 - gii_2010)
+## load data
+World <- st_read("World_Countries_Generalized_Shapefile/World_Countries_Generalized.shp")
+HDI <-  read.csv("HDR23-24_Composite_indices_complete_time_series.csv")
 
-##join
-countries <- countries%>%
-  merge(.,
-        gii2,
-        by.x="country", 
-        by.y="country")
+
+## column names 
+HDIcols<- HDI %>%
+  clean_names() %>%
+  select(iso3, country, gii_2019, gii_2010) %>%
+  mutate(difference = gii_2019-gii_2010) %>%
+  mutate(iso_code = countrycode(country, origin = 'country.name', destination = 'iso2c')) %>%
+  mutate(iso_code2 = countrycode(iso3, origin ='iso3c', destination = 'iso2c'))
+
+
+## join
+Join_HDI <- World %>% 
+  clean_names() %>%
+  left_join(., 
+            HDIcols,
+            by = c("aff_iso" = "iso_code"))
+
+
+## problem
+Join_HDI_GB<-Join_HDI %>%
+  filter(aff_iso =="GB")
+
 
 ##plot
 tmap_mode("plot")
 tmap_options(check.and.fix = TRUE)
 
-map_2010 <- qtm(countries,
-                fill = "gii_2010",
-                fillNA = "gray90",         
-                    fill.palette = "Reds")
-map_2010
-
-map_2019 <- qtm(countries,
-                fill = "gii_2019",
-                fillNA = "gray90",         
-                fill.palette = "Reds")
-map_2019
-
-map_difference <- qtm(countries,
+map_difference <- qtm(Join_HDI,
                       fill = "difference",
                       fillNA = "gray90",         
                       fill.palette = "RdBu",   
-                      fill.midpoint = median(countries$difference, na.rm = TRUE)) 
+                      fill.midpoint = median(Join_HDI$difference, na.rm = TRUE)) 
 
 map_difference
 
